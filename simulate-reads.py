8752e1183bd8f54667c76698dccaa7642554834e
margarytavityaz <margaryta.vityaz@gmail.com>
2017-02-27 20:27:19 -0800

    Fixed shape of dataframe

--- simulate-reads.py
+++ simulate-reads.py
@@ -10,6 +10,7 @@ read_number = 1000
 template_length = 53
 genome_length = 5000
 copy_numbers = [1, 2, 3, 4, 5, 6, 7]
+max_cn = 7
 
 #simulation params
 simulation_number = 500
@@ -22,6 +23,7 @@ simulation_values = pd.DataFrame(index=range(0, simulation_number * 7),
                                  columns=["cn", "value"], dtype='int')
 
 print simulation_values
+row = 0
 for copy_number in copy_numbers:
     print "copy number", copy_number
     for i in range(simulation_number):
@@ -33,14 +35,12 @@ for copy_number in copy_numbers:
         read_count = compute_read_count(generated_reads, read_length,
                                         generated_template, template_length, distance_threshold)
 
-        simulation_values.set_value(i , copy_number, read_count)
+        simulation_values.set_value(row, "cn", copy_number)
 
-        simulation_values.set_value(i, copy_number, read_count)
+        simulation_values.set_value(row, "value", read_count)
+        row = row + 1
 
 
 print simulation_values
 
-print list(simulation_values)
-cn1 = simulation_values[1].tolist()
-print cn1
 
